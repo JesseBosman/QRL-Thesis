3428af8211ed668d6fad356bf25a10159f0ba11c
JesseBosman <113833400+JesseBosman@users.noreply.github.com>
2023-09-08 16:56:58 +0200

    Update REINFORCE.py

--- REINFORCE.py
+++ REINFORCE.py
@@ -12,3 +12,39 @@ from collections import deque, defaultdict
 import matplotlib.pyplot as plt
 from cirq.contrib.svg import SVGCircuit
 
+class reinforce_agent()
+  def __innit__(self):
+    pass
+
+  @tf.function
+  def update(states, actions, returns, model):
+    # need to be able to specify which optimizer and which model
+    states = tf.convert_to_tensor(states)
+    actions = tf.convert_to_tensor(actions)
+    returns = tf.convert_to_tensor(returns)
+
+    with tf.GradientTape() as tape:
+        tape.watch(model.trainable_variables)
+        logits = model(states)
+        p_actions = tf.gather_nd(logits, actions)
+        log_probs = tf.math.log(p_actions)
+        loss = tf.math.reduce_sum(-log_probs * returns) / batch_size
+    grads = tape.gradient(loss, model.trainable_variables)
+    for optimizer, w in zip([optimizer_in, optimizer_var, optimizer_out], [w_in, w_var, w_out]):
+        optimizer.apply_gradients([(grads[w], model.trainable_variables[w])])
+
+
+  def compute_returns(self, rewards_history, gamma):
+    """Compute discounted returns with discount factor `gamma`."""
+    returns = []
+    discounted_sum = 0
+    for r in rewards_history[::-1]:
+        discounted_sum = r + gamma * discounted_sum
+        returns.insert(0, discounted_sum)
+
+    # Normalize them for faster and more stable learning
+    returns = np.array(returns)
+    returns = (returns - np.mean(returns)) / (np.std(returns) + 1e-8)
+    returns = returns.tolist()
+
+    return returns
